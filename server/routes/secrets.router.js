31f428e2dc69eb710a6a1a28a2fb4b8e16a21486
Maxwell Todd <mtmaxtodd@gmail.com>
2019-02-06 13:46:26 -0600

    completed base mode.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,23 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        // They were authenticated! User may do the next thing
+        // Note! They may not be Authorized to do all things
+        // res.send(req.user);
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+            pool.query(`SELECT * FROM "secret" WHERE secrecy_level <= ${req.user.clearance_level};`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    } else {
+        // They are not authenticated.
+        res.sendStatus(403);
+    }
+
 });
+
 
 module.exports = router;
